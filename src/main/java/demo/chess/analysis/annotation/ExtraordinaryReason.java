d45f82043baa09b67f3fbb74836f630d7f1ff836
romansen47 <romansen47@gmail.com>
2026-09-12 11:31:26 +0200

    Introduce simplified extraordinary move model

---
+++ src/main/java/demo/chess/analysis/annotation/ExtraordinaryReason.java
@@ -0,0 +1,10 @@
+package demo.chess.analysis.annotation;
+
+/**
+ * Concrete, explainable reason for assigning "!!".
+ */
+public enum ExtraordinaryReason {
+    DEEP_DISCOVERY,
+    MATERIAL_SACRIFICE,
+    DEEP_DISCOVERY_AND_MATERIAL_SACRIFICE
+}
