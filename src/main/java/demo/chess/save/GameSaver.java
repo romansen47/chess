3b4bdb9f22dab35272fecac0f986367094bafb17
romansen47 <romansen47@gmail.com>
2026-09-12 16:08:31 +0200

    Keep user comments valid in PGN export

--- src/main/java/demo/chess/save/GameSaver.java
+++ src/main/java/demo/chess/save/GameSaver.java
@@ -167,7 +167,7 @@ public class GameSaver {
     }
 
     private String sanitizeComment(String comment) {
-        return comment.replace('}', ']');
+        return comment.replace('{', '[').replace('}', ']');
     }
 
     /**
